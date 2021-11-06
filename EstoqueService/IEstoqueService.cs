d0f867e8d2cd5acae53b367ede8e58943eeb07ce
Samuel Pereira <samuelpereira@gec.inatel.br>
2021-11-06 18:49:05 -0300

    create EstoqueService and define data contract

---
+++ EstoqueService/IEstoqueService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+namespace Products
+{
+    // Data contract describing the details of a product passed to client applications
+    [DataContract]
+    public class ProductData
+    {
+        [DataMember]
+        public string NumeroProduto;
+        
+        [DataMember]
+        public string NomeProduto;
+
+        [DataMember]
+        public string DescricaoProduto;
+
+        [DataMember]
+        public int EstoqueProduto;
+    }
+}
