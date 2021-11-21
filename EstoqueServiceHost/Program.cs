f977eba7c8836e701ec6ac02a9206d806e3ee17b
Samuel Pereira <samuelpereira@gec.inatel.br>
2021-11-20 22:14:20 -0300

    create service host

---
+++ EstoqueServiceHost/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using Products;
+
+namespace EstoqueServiceHost
+{
+    class Program
+    {
+         static void Main(string[] args)
+         {
+            ServiceHost productsServiceHost = new ServiceHost(typeof(ProductsService));
+            productsServiceHost.Open();
+            Console.WriteLine("Service Running");
+            Console.ReadLine();
+            Console.WriteLine("Service Stopping");
+            productsServiceHost.Close();
+        }
+    }
+}
