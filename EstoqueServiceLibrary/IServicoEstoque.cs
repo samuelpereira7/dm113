d3b5c9c7754166cb10b44c60a2d4040cc7f7f7d7
Samuel Pereira <samuelpereira@gec.inatel.br>
2021-11-20 22:13:08 -0300

    create service library

---
+++ EstoqueServiceLibrary/IServicoEstoque.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+namespace ServicoEstoque
+{
+    // Service contract describing the operations provided by the WCF service (v1)
+    [ServiceContract(Namespace = "http://projetoavaliativo.dm113/01", Name = "IServicoEstoque")]
+    public interface IEstoqueService
+    {
+        [OperationContract]
+        List<string> ListarProdutos();
+
+        [OperationContract]
+        bool IncluirProduto(ProductData Produto);
+
+        [OperationContract]
+        bool RemoverProduto(string NumeroProduto);
+
+        [OperationContract]
+        int ConsultarEstoque(string NumeroProduto);
+
+        [OperationContract]
+        bool AdicionarEstoque(string NumeroProduto, int Quantidade);
+
+        [OperationContract]
+        bool RemoverEstoque(string NumeroProduto, int Quantidade);
+
+        [OperationContract]
+        ProductData VerProduto(string NumeroProduto);
+    }
+
+    // Service contract describing the operations provided by the WCF service (v2)
+    [ServiceContract(Namespace = "http://projetoavaliativo.dm113/02", Name = "IServicoEstoqueV2")]
+    public interface IEstoqueServiceV2
+    {
+        [OperationContract]
+        bool AdicionarEstoque(string NumeroProduto, int Quantidade);
+
+        [OperationContract]
+        bool RemoverEstoque(string NumeroProduto, int Quantidade);
+
+        [OperationContract]
+        int ConsultarEstoque(string NumeroProduto);
+    }
+
+    // Data contract describing the details of a product passed to client applications
+    [DataContract]
+    public class ProductData
+    {
+        [DataMember]
+        public string NumeroProduto;
+
+        [DataMember]
+        public string NomeProduto;
+
+        [DataMember]
+        public string DescricaoProduto;
+
+        [DataMember]
+        public int EstoqueProduto;
+    }
+}
